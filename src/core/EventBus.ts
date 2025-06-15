8b204e1a0399dce77cd92ecc3567363eab2cc239
SEREZHKA3310 <bargansergei333@gmail.com>
2025-06-15 21:58:10 +0300

    base EventBus (#3)
    * base EventBus
    
    * more typification and export class
    
    * Update EventBus.ts
    
    ---------
    
    Co-authored-by: Ivan <101219328+perspest-prog@users.noreply.github.com>

---
+++ src/core/EventBus.ts
@@ -0,0 +1,32 @@
+type VoidFunction = (...args: any[]) => void
+
+class EventBus {
+  private readonly listeners = new Map<string, Set<VoidFunction>>()
+  
+  public on(event: string, callback: VoidFunction) {
+    let getEntries = this.listeners.get(event)
+      if (!getEntries) {
+          this.listeners.set(event, new Set)
+      }
+      getEntries!.add(callback)
+  }
+  
+  public off(event: string, callback: VoidFunction) {
+    let getEntries = this.listeners.get(event)
+    if (!getEntries) {
+      throw new Error('Неизвестный ключ')
+    }
+    else {
+      getEntries!.delete(callback)
+    }
+  }
+  
+  public emit(event: string) {
+    let getEntries = this.listeners.get(event)
+    if (getEntries) {
+      getEntries.forEach((func) => {func()})
+    }
+  }
+}
+
+export default EventBus
