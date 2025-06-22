36c542d3b0140dadc5be676c4441a31b38872681
Sergei Bargan <bargansergei333@gmail.com>
2025-06-22 16:06:38 +0300

    connection with Component and EventBus

--- src/core/Component.ts
+++ src/core/Component.ts
@@ -3,19 +3,30 @@ import EventBus from './EventBus'
 type State<P> = any
 type Children<P> = any
 
-interface Props {}
+interface Props {callEventBus: any}
+
+enum Methods {
+  'didMount' = 'didMount',
+  'DidUpdate' = 'didUpdate',
+  'WillUnmount' = 'WillUnmount'
+}
 
 abstract class Component<P extends Props> {
   protected state: State<P>
   protected children: Children<P>
+  callEventBus = new EventBus()
 
   constructor(props: P) {
     const { state, children } = Component.getStateAndChildren(props)
     
     this.state = state
     this.children = children
+    
+    this.callEventBus.on(Methods.didMount, this._componentDidMount)
+    this.callEventBus.on(Methods.DidUpdate, this._componentDidUpdate)
+    this.callEventBus.on(Methods.WillUnmount, this._componentWillUnmount)
   }
-
+  
   private static getStateAndChildren<P extends Props>(props: P) {
     const state = {} as State<P>
     const children = {} as Children<P>
@@ -31,6 +42,23 @@ abstract class Component<P extends Props> {
     }
     
     return { state, children }
+  }
+  protected componentDidMount<P extends Props>() {
+
+  }
+  protected componentDidUpdate<P extends Props>() {
+
+  }
+  protected componentWillUnmount<P extends Props>() {
+
+  }
+  private _componentDidMount<P extends Props>() {
+
+  }
+  private _componentDidUpdate<P extends Props>() {
+  }
+  private _componentWillUnmount<P extends Props>() {
+
   }
 }
 
