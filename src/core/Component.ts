f58451b8e095f466c942d93b24317015ad212e66
Ivan Avilov <ivan.avilov.0606@mail.ru>
2026-02-08 21:40:10 +0300

    lala

--- src/core/Component.ts
+++ src/core/Component.ts
@@ -40,8 +40,8 @@ abstract class Component<P extends Props = any> {
   constructor({ events, ...props }: P) {
     const { state, children } = Component.getStateAndChildren(props)
 
-    this.state = this.makeProxyState(state)
-    this.children = children
+    this.state = this.makeProxy(state)
+    this.children = this.makeProxy(children)
     this.events = this.makeProxyEvents(events || {})
 
     this.eventBus.on(PHASES.MOUNT, this._componentDidMount.bind(this))
@@ -68,14 +68,13 @@ abstract class Component<P extends Props = any> {
     return { state, children }
   }
 
-  private makeProxyState(state: State<P>) {
-    const self = this
-
+  private makeProxy(state: State<P> | Children<P>) {
     return new Proxy(state, {
       set: (target, prop, value) => {
-        target[prop] = value
-        self.eventBus.emit(PHASES.UPDATE)
-        return true
+        const result = Reflect.set(target, prop, value)
+        this.eventBus.emit(PHASES.UPDATE)
+
+        return result
       },
     })
   }
@@ -171,11 +170,11 @@ abstract class Component<P extends Props = any> {
     this.componentWillUnmount()
   }
 
-  protected componentDidMount() { }
+  protected componentDidMount() {}
 
-  protected componentDidUpdate() { }
+  protected componentDidUpdate() {}
 
-  protected componentWillUnmount() { }
+  protected componentWillUnmount() {}
 
   protected abstract render(): TemplateDelegate
 
